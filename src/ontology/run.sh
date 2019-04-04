3a76c23e4aeaec9e45c9942534378cd987fc40c7
Shahim Essaid <shahim@essaid.com>
2019-04-04 11:53:58 -0700

    fix

--- src/ontology/run.sh
+++ src/ontology/run.sh
@@ -9,11 +9,4 @@
 # we therefore map the whole repo (../..) to a docker volume.
 #
 # See README-editors.md for more details.
-
-set -x
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
-sudo apt-get update
-sudo apt install moreutils
-
-docker run --memory=8g -e ROBOT_JAVA_ARGS=-Xmx7G -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull "$@" | ts -s %M:%S
+docker run --memory=8g -e ROBOT_JAVA_ARGS=-Xmx7G -v $PWD/../../:/work -w /work/src/ontology --rm -ti obolibrary/odkfull "$@"
